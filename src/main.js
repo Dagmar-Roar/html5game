4ea662f9ac165a1a43d7604d5c093a215dcd339b
Dagmar-Roar <iamerwanaubert@gmail.com>
2018-03-22 18:10:41 +0100

    'commit'

--- src/main.js
+++ src/main.js
@@ -1,40 +1,37 @@
-let clickers = 50;
-let startTime = Date.now();
+const canvas = document.querySelector("#board canvas");
+const ctx = canvas.getContext("2d");
+const { width: w, height: h } = canvas;
 
-// position element in the DOM
-function sync (dom, pos) {
-    dom.style.left = `${pos.x}px`;
-    dom.style.top = `${pos.y}px`;
-}
+// Game setup code
+ctx.strokeStyle = "#fff";
+ctx.font = "30pt monospace";
 
+const speed = 64;
+let p1 = 0;
+let p2 = 0;
 
-function addClicker () {
-    const pos = {
-    x: Math.random() * 500,
-    y: Math.random() * 300
-    };
-    const img = new Image();
-    img.src = "res/img/rick.png";
-    img.style.position = "absolute";
-    img.addEventListener("click", removeClicker, false);
-    document.querySelector("#board").appendChild(img);
-    sync(img, pos);
-}
-    
-function removeClicker (e) {
-    e.target.parentNode.removeChild(e.target);
-    clickers--;
-    checkGameOver();
-}
+let dt = 0;
+let last = 0;
+function loopy(ms) {
+  requestAnimationFrame(loopy);
 
-function checkGameOver(){  
-    document.querySelector("#remain").innerHTML = clickers;
-    if (clickers === 0) {
-    const taken = Math.round((Date.now() - startTime) / 1000);
-    alert(`De-rick-ed in ${taken} seconds!`);
-    }
-}
+  const t = ms / 1000; // Let's work in seconds
+  dt = t - last;
+  last = t;
 
-for (let i=0; i < clickers; i++){
-    addClicker();
+  // Game logic code
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, w, h);
+  ctx.strokeText(`Frame length: ${(dt * 1000).toFixed(2)} ms`, 70, 50);
+  ctx.strokeText(`Total time: ${t.toFixed(2)}`, 70, 90);
+
+  p1 += speed * dt;
+  p2 += speed * (1 / 60);
+  if (p1 > w) p1 -= w + 50;
+  if (p2 > w) p2 -= w + 50;
+
+  ctx.fillStyle = "#f00";
+  ctx.fillRect(p1, 120, 50, 50);
+  ctx.fillRect(p2, 190, 50, 50);
 }
+requestAnimationFrame(loopy);
